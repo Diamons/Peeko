47fc26d8df0bffe16e3aff5ab30531b123512e03
Shahruk Khan <shahruksemail@gmail.com>
2014-05-15 13:20:08 -0400

    Test

--- public/phpMyAdmin/config.php
+++ public/phpMyAdmin/config.php
@@ -1,64 +1,158 @@
 <?php
-/*
-
-SQL Buddy - Web based MySQL administration
-http://www.sqlbuddy.com/
-
-config.php
-- options for customizing sql buddy
-
-MIT license
-
-2008 Calvin Lough <http://calv.in>
-
-*/
-
-
-/* The values below are for the login.php page */
-
-$sbconfig['DefaultAdapter'] = "mysql";
-$sbconfig['DefaultHost'] = "localhost";
-$sbconfig['DefaultUser'] = "root";
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <http://docs.phpmyadmin.net/>.
+ *
+ * @package PhpMyAdmin
+ */
 
 /*
-* If you want to enable automatic login, you can include your password below. To 
-* automatically connect to SQLite, include the filename of the database instead.
-* Note: This is generally not recommended because it means that anyone with the 
-* proper url will have access to your data. It should only be used on machines
-* that are not accessible from the internet (local testing boxes) or if you have
-* set up some other form of authentication. Use as your own discretion.
-*/
-
-// MySQL
- $sbconfig['DefaultPass'] = "aCC23zzzY23";
-
-// SQLite
-// $sbconfig['DefaultDatabase'] = "";
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie
+ */
+$cfg['blowfish_secret'] = 'a8b7c6d1sada!asdada'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
-* By default, when you view the homepage of your SQL Buddy installation, a check is
-* performed to see if a newer version of the application is available. No personal 
-* information is sent about your installation and the response from the server is sent
-* as plain text. If, for some reason, you want to disable this behaviour, set this option
-* to true to disable automatic checking for updates. To learn about new versions, you can
-* always check the projects website at http://www.sqlbuddy.com/
-*/
-
-$sbconfig['EnableUpdateCheck'] = true;
+ * Servers configuration
+ */
+$i = 0;
 
 /*
-* This controls how many rows are displayed at once on the browse tab. If you are on a local
-* machine or have a fast internet connection, you could increase this value.
-*/
-
-$sbconfig['RowsPerPage'] = 100;
+ * First server
+ */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 /*
-* When set to true, the server will attempt to compress all content before it is sent to the 
-* browser. Although unlikely, there is a chance that using gzip will cause issues on certain setups. 
-* If you are having trouble getting pages to load properly, you could try disabling gzip.
-*/
+ * phpMyAdmin configuration storage settings.
+ */
 
-$sbconfig['EnableGzip'] = true;
+/* User used to manipulate with storage */
+// $cfg['Servers'][$i]['controlhost'] = '';
+// $cfg['Servers'][$i]['controlport'] = '';
+// $cfg['Servers'][$i]['controluser'] = 'pma';
+// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
 
+/* Storage database and tables */
+// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+// $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+// $cfg['Servers'][$i]['relation'] = 'pma__relation';
+// $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+// $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+// $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+// $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+// $cfg['Servers'][$i]['history'] = 'pma__history';
+// $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+// $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+// $cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
+// $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+// $cfg['Servers'][$i]['recent'] = 'pma__recent';
+// $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+// $cfg['Servers'][$i]['users'] = 'pma__users';
+// $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+// $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+// $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+/* Contrib / Swekey authentication */
+// $cfg['Servers'][$i]['auth_swekey_config'] = '/etc/swekey-pma.conf';
+
+/*
+ * End of servers configuration
+ */
+
+/*
+ * Directories for saving/loading files from server
+ */
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+
+/**
+ * Whether to display icons or text or both icons and text in table row
+ * action segment. Value can be either of 'icons', 'text' or 'both'.
+ */
+//$cfg['RowActionType'] = 'both';
+
+/**
+ * Defines whether a user should be displayed a "show all (records)"
+ * button in browse mode or not.
+ * default = false
+ */
+//$cfg['ShowAll'] = true;
+
+/**
+ * Number of rows displayed when browsing a result set. If the result
+ * set contains more rows, "Previous" and "Next".
+ * default = 30
+ */
+//$cfg['MaxRows'] = 50;
+
+/**
+ * disallow editing of binary fields
+ * valid values are:
+ *   false    allow editing
+ *   'blob'   allow editing except for BLOB fields
+ *   'noblob' disallow editing except for BLOB fields
+ *   'all'    disallow editing
+ * default = blob
+ */
+//$cfg['ProtectBinary'] = 'false';
+
+/**
+ * Default language to use, if not browser-defined or user-defined
+ * (you find all languages in the locale folder)
+ * uncomment the desired line:
+ * default = 'en'
+ */
+//$cfg['DefaultLang'] = 'en';
+//$cfg['DefaultLang'] = 'de';
+
+/**
+ * default display direction (horizontal|vertical|horizontalflipped)
+ */
+//$cfg['DefaultDisplay'] = 'vertical';
+
+
+/**
+ * How many columns should be used for table display of a database?
+ * (a value larger than 1 results in some information being hidden)
+ * default = 1
+ */
+//$cfg['PropertiesNumColumns'] = 2;
+
+/**
+ * Set to true if you want DB-based query history.If false, this utilizes
+ * JS-routines to display query history (lost by window close)
+ *
+ * This requires configuration storage enabled, see above.
+ * default = false
+ */
+//$cfg['QueryHistoryDB'] = true;
+
+/**
+ * When using DB-based query history, how many entries should be kept?
+ *
+ * default = 25
+ */
+//$cfg['QueryHistoryMax'] = 100;
+
+/**
+ * Should error reporting be enabled for JavaScript errors
+ *
+ * default = 'ask'
+ */
+//$cfg['SendErrorReports'] = 'ask';
+
+/*
+ * You can find more configuration options in the documentation
+ * in the doc/ folder or at <http://docs.phpmyadmin.net/>.
+ */
 ?>
