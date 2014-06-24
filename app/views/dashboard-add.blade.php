718f30c588eaffb8f05b2bf0e86d5c76e23091f3
Shahruk Khan <shahruksemail@gmail.com>
2014-06-24 01:47:42 -0400

    Fix

--- app/views/dashboard-add.blade.php
+++ app/views/dashboard-add.blade.php
@@ -49,9 +49,9 @@
 	<div class="wrap">
 		<?php var_dump($store); ?>
 		@if(isset($store))
-			Form::model($store, array('url' => 'business/add/'.$store->id));
+			{{ Form::model($store, array('url' => 'business/add/'.$store->id)); }}
 		@else
-			Form::open(array('url' => 'business/add'));
+			{{ Form::open(array('url' => 'business/add')); }}
 		@endif
 		<div id="locationField">
       		<input id="autocomplete" placeholder="Enter your address" onFocus="geolocate()" type="text" />
