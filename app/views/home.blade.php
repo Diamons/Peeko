9760cd169d2a405793865a98c3472e80a98e5d25
Shahruk Khan <shahruksemail@gmail.com>
2014-06-25 13:00:36 -0400

    iOS updated

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
 	<div id="peekomain">
-		<h1>Find Exclusive Deals Nearby</h1>
+		<h1>Instant Local Deals</h1>
 		<div id="deals" class="clearfix">
 			<div class="deal-container">
 				<div class="single-deal animated fadeInDown">
@@ -20,13 +20,16 @@
 					<img class="icon" src="/img/icons/drink.png" />Happy Hour Specials
 				</div>
 			</div>
+
 			<div class="about">
-				<img src="/img/iosappstore.png" class="iosappstore" alt="Peeko Coming Soon to the iTunes App Store for iPhone" />
+				<a href="https://itunes.apple.com/us/app/peeko/id781198595?ls=1&mt=8"><img src="/img/app_store_badge.png" class="appstore" alt="Peeko On the iTunes App Store for iPhone" /></a>
+				<a target="_blank" href="https://docs.google.com/forms/d/1ZCeyJ5bl2ZiM0NMEqSihIsz19lh3ZscLa38e3FDoafs/viewform"><img src="http://eutobaptist.com/wp-content/uploads/2014/04/google-play.png" class="appstore" alt="Peeko On the iTunes App Store for iPhone" /></a>
 				<p>Peeko is an iPhone app that brings you deals at stores nearby.</p>
 				<p>Businesses offer discounts to local customers to grow their business.</p>
 				<p>No coupons or vouchers to buy beforehand. Simply walk in, present the deal, and redeem.</p>
 				<p>Save money and take advantage of great promotions nearby!</p>
 			</div>
 		</div>
+		<iframe src="https://docs.google.com/forms/d/1ZCeyJ5bl2ZiM0NMEqSihIsz19lh3ZscLa38e3FDoafs/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 	</div>
 @stop
