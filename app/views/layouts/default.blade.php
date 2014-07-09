dae6e25822609757b0029c7fef651544e2977017
Shahruk Khan <shahruksemail@gmail.com>
2014-07-09 00:19:51 -0400

    Google Analytics to Heap SAnalytis

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,8 +2,7 @@
 <html>
 <head>
 <title>{{ $title }} - Peeko</title>
-{{ HTML::style('//fonts.googleapis.com/css?family=Montserrat:400,700'); }}
-<link href='http://fonts.googleapis.com/css?family=Muli:300,400' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic' rel='stylesheet' type='text/css'>
 {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'); }}
 {{ HTML::style('css/stylesheet.css'); }}
 {{ HTML::style('css/animate.css'); }}
@@ -38,15 +37,9 @@
 {{ HTML::script('http://code.jquery.com/jquery-2.1.0.min.js'); }}
 {{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js'); }}
 @yield('footerscripts')
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-2509553-11', 'peekoapp.com');
-  ga('send', 'pageview');
-
+<script type="text/javascript">
+  window.heap=window.heap||[];heap.load=function(a){window._heapid=a;var b=document.createElement("script");b.type="text/javascript",b.async=!0,b.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.heapanalytics.com/js/heap.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c);var d=function(a){return function(){heap.push([a].concat(Array.prototype.slice.call(arguments,0)))}},e=["identify","track"];for(var f=0;f<e.length;f++)heap[e[f]]=d(e[f])};
+  heap.load("1094769445");
 </script>
 </body>
 </html>
